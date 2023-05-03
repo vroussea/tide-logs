a91f73c45b3eb07c9b24836a541d984bb85ce045
Vlad Stepanov <8uk.8ak@gmail.com>
2023-05-03 12:37:45 +0400

    extract app to separate lib crate, add dotenv

--- tide-logs-raw/src/main.rs
+++ tide-logs-raw/src/main.rs
@@ -1,30 +1,10 @@
-use tide::prelude::*;
-use tide::Request;
-
-#[derive(Debug, Deserialize)]
-struct Body {
-    something: Option<String>,
-}
-
 #[async_std::main]
 async fn main() -> tide::Result<()> {
+    dotenvy::dotenv()?;
     pretty_env_logger::init();
 
-    let mut app = tide::new();
-
-    app.with(tide::log::LogMiddleware::new());
-    app.at("/").get(|_| async { Ok("Hello, world!") });
-    app.at("/helloworld").post(hello_world);
+    let app = tide_app::app();
     app.listen("127.0.0.1:8080").await?;
 
     Ok(())
-}
-
-async fn hello_world(mut req: Request<()>) -> tide::Result {
-    let Body { something } = req.body_json().await?;
-    if let Some(text) = something {
-        Ok(format!("Hello World ! {}\n", text).into())
-    } else {
-        Ok("Hello World !\n".into())
-    }
 }
--- tide-logs-shuttle/src/main.rs
+++ tide-logs-shuttle/src/main.rs
@@ -1,26 +1,6 @@
-use tide::{convert::Deserialize, Request};
-
-#[derive(Debug, Deserialize)]
-struct Body {
-    something: Option<String>,
-}
-
 #[shuttle_runtime::main]
 async fn tide() -> shuttle_tide::ShuttleTide<()> {
-    let mut app = tide::new();
-
-    app.with(tide::log::LogMiddleware::new());
-    app.at("/").get(|_| async { Ok("Hello, world!") });
-    app.at("/helloworld").post(hello_world);
+    let app = tide_app::app();
 
     Ok(app.into())
-}
-
-async fn hello_world(mut req: Request<()>) -> tide::Result {
-    let Body { something } = req.body_json().await?;
-    if let Some(text) = something {
-        Ok(format!("Hello World ! {}\n", text).into())
-    } else {
-        Ok("Hello World !\n".into())
-    }
 }
